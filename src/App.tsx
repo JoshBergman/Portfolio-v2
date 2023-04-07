92e20b0b61be8f2409caf4537ab440a113ccd8d8
JoshBergman <111944876+JoshBergman@users.noreply.github.com>
2023-04-07 12:09:00 -0500

    TSA - Analytics API

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import './App.css';
-import About from './Components/Sections/About';
-import Contact from './Components/Sections/Contact';
-import Landing from './Components/Sections/Landing';
-import Projects from './Components/Sections/Project';
-import Skills from './Components/Sections/Skills';
-import CopyRight from './Components/UI/CopyRight';
+import React, { useEffect } from "react";
+
+import "./App.css";
+import About from "./Components/Sections/About";
+import Contact from "./Components/Sections/Contact";
+import Landing from "./Components/Sections/Landing";
+import Projects from "./Components/Sections/Project";
+import Skills from "./Components/Sections/Skills";
+import CopyRight from "./Components/UI/CopyRight";
 
 function App() {
+  //just a lil data mining
+  useEffect(() => {
+    try {
+      fetch("http://too-simple-analytics.herokuapp.com/std/view/JoshPortfolio");
+    } catch (err) {}
+  }, []);
   return (
     <React.Fragment>
       <Landing />
